c447d438125f7af21bc6047eb3c0d5ee02d39814
DevFrog92 <yongsu1481@gmail.com>
2021-04-25 23:43:17 +0900

    [0425] User body parser for form data and use routing

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 // import express
 const express = require("express");
+// express view engine
+const nunjuck = require("nunjucks");
+//
+const logger = require("morgan");
+//
+const bodyParser = require("body-parser");
+
 // setting express
 const app = express();
 // port
 const port = 3000;
+
+nunjuck.configure("template", {
+  autoescape: true, // cross site 공격을 무효화 시키기 위해서 사용한다.
+  express: app,
+});
+
+// Middleware settings -> router 설정 전에 정의
+app.use(logger("dev"));
+// bodyParser setting -> middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+/*
+request에 body 변수를 추가했다.
+app.use((req,res,next) => {
+  req.body = {
+
+  }
+})
+*/
+function vipMiddleware(req, res, next) {
+  console.log("최우선 미들웨어");
+  next();
+}
+
 // router
 const admin = require("./routes/admin");
 const contacts = require("./routes/contacts");
@@ -18,7 +49,7 @@ app.get("/test", (req, res) => {
 });
 
 // middleware
-app.use("/admin", admin);
+app.use("/admin", vipMiddleware, admin);
 app.use("/contacts", contacts);
 
 // open server
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,46 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+//
+function testMiddleware(req, res, next) {
+  console.log("First middleware");
+  next();
+}
+
+function testMiddleware2(req, res, next) {
+  console.log("Second middleware");
+  next();
+}
+
+// ex )
+// function loginRequired(req,res,next) {
+//   if(로그인이 되어있지 않으면){
+//     res.redirect('로그인 창으로')
+//   }else {
+//     next();
+//   }
+// }
+
+// moddleware를 한번 거치고 제어권을 next로 다음으로 넘긴다
+router.get("/", testMiddleware, testMiddleware2, (req, res) => {
   res.send("admin 이후의 url");
 });
 
 router.get("/products", (req, res) => {
-  res.send("admin products 이후의 url"); // text
+  // res.send("admin products 이후의 url"); // text
+  // tempalte 이후의 경로
+  res.render("admin/products.html", {
+    message: "<h1>This is DevFrog</h1>",
+    online: "express online",
+  });
+});
+
+router.get("/products/write", (req, res) => {
+  res.render("admin/write.html");
+});
+
+router.post("/products/write", (req, res) => {
+  res.send(req.body);
 });
 
 module.exports = router;
