ab8596c9323dd90b1377d63a90fc6fd3becc3970
DevFrog92 <yongsu1481@gmail.com>
2021-04-26 00:46:58 +0900

    [0426] Complete express lecture

--- app.js
+++ app.js
@@ -1,58 +1,91 @@
-// import express
-const express = require("express");
-// express view engine
-const nunjuck = require("nunjucks");
-//
-const logger = require("morgan");
-//
-const bodyParser = require("body-parser");
+const express = require('express');
+const nunjucks = require('nunjucks');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-// setting express
-const app = express();
-// port
-const port = 3000;
 
-nunjuck.configure("template", {
-  autoescape: true, // cross site 공격을 무효화 시키기 위해서 사용한다.
-  express: app,
-});
+class App {
 
-// Middleware settings -> router 설정 전에 정의
-app.use(logger("dev"));
-// bodyParser setting -> middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-/*
-request에 body 변수를 추가했다.
-app.use((req,res,next) => {
-  req.body = {
+    constructor () {
+        this.app = express();
+        
+        // 뷰엔진 셋팅
+        this.setViewEngine();
 
-  }
-})
-*/
-function vipMiddleware(req, res, next) {
-  console.log("최우선 미들웨어");
-  next();
+        // 미들웨어 셋팅
+        this.setMiddleWare();
+
+        // 정적 디렉토리 추가
+        this.setStatic();
+
+        // 로컬 변수
+        this.setLocals();
+
+        // 라우팅
+        this.getRouting();
+
+        // 404 페이지를 찾을수가 없음
+        this.status404();
+
+        // 에러처리
+        this.errorHandler();
+
+
+    }
+
+
+    setMiddleWare (){
+        
+        // 미들웨어 셋팅
+        this.app.use(logger('dev'));
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+
+    }
+
+    setViewEngine (){
+
+        nunjucks.configure('template', {
+            autoescape: true,
+            express: this.app
+        });
+
+    }
+
+
+    setStatic (){
+        this.app.use('/uploads', express.static('uploads'));
+    }
+
+    setLocals(){
+
+        // 템플릿 변수
+        this.app.use( (req, res, next) => {
+            this.app.locals.isLogin = true;
+            this.app.locals.req_path = req.path;
+            next();
+        });
+
+    }
+
+    getRouting (){
+        this.app.use(require('./controllers'))
+    }
+
+    status404() {        
+        this.app.use( ( req , res, _ ) => {
+            res.status(404).render('common/404.html')
+        });
+    }
+
+    errorHandler() {
+
+        this.app.use( (err, req, res,  _ ) => {
+            res.status(500).render('common/500.html')
+        });
+    
+    }
+
 }
 
-// router
-const admin = require("./routes/admin");
-const contacts = require("./routes/contacts");
-
-// GET / generat the url
-app.get("/", (req, res) => {
-  res.send("This is DevFrog by Express");
-});
-// GET /test
-app.get("/test", (req, res) => {
-  res.send("This is test page");
-});
-
-// middleware
-app.use("/admin", vipMiddleware, admin);
-app.use("/contacts", contacts);
-
-// open server
-app.listen(port, () => {
-  console.log(`Express listening on port ${port}`);
-});
+module.exports = new App().app;
